5f1ce60c7786880d155d253c8a97b03cac51b605
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-08-09 20:36:28 -0500

    Created intitial html and js files. Started on Questions.

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = [{
+    question1: "What is an array?",
+    choices1: ["A special variable which can hold more than 1 value", "A variable that can store a single item of text", "A single number", "A way to create a proccess that will perform a task for you."]
+}]
+
+let button = document.querySelector(".btn btn-primary brn-lg")
+
+button.addEventListener("click", function(event) {
+
+})
