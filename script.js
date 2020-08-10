341f4c98a69ea20de64a343639ac38c39d98d392
Evan Mackay <evanmackay@Evans-MacBook-Pro.local>
2020-08-09 21:00:19 -0500

    started with question and created some html elements.

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-let questions = [{
-    question1: "What is an array?",
-    choices1: ["A special variable which can hold more than 1 value", "A variable that can store a single item of text", "A single number", "A way to create a proccess that will perform a task for you."]
+let allQuestions = [{
+    question: "What is an array?",
+    choiceA: "A special variable which can hold more than 1 value", 
+    choiceB: "A variable that can store a single item of text", 
+    choiceC: "A single number",
+    choiceD: "A way to create a proccess that will perform a task for you.",
+    correctAnswer: "a"
+},
+{
+    question: "What would farmAnimals.length be for this array? farmAnimals = ['cow', 'chicken', 'pig', 'goat']?",
+    choiceA: "3",
+    choiceB: "4",
+    choiceC: "2",
+    choiceD: "5",
+    correctAnswer: "b"
+},
+{
+    question: "What does API stand for?",
+    choiceA: "Aritficial Proportions Interface",
+    choiceB: "Application Property Inspector",
+    choiceC: "Aggregating Personal Identificaiton",
+    choiceD: "Application Programmnig Interface",
+    correctAnswer: "d"
+},
+{
+    question: "What are the 3 fundamental programs we use for front-end web development?",
+    choiceA: "HTML, Bootstrap, Jquery",
+    choiceB: "Github, StackOverflow, MDN",
+    choiceC: "HTML, CSS, JavaScript",
+    choiceD: "JavaScript, React, Node.js",
+    correctAnswer: "c"
+},
+{
+    question: "Which of these would I use to apply text to an element in an HTML file using JavaScript?",
+    choiceA: ".createText",
+    choiceB: ".textInclude",
+    choiceC: ".textContent",
+    choiceD: ".addText",
+    correctAnswer: "c"
 }]
+
+
+
 
 let button = document.querySelector(".btn btn-primary brn-lg")
 
